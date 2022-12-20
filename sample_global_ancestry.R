ae1898cfaa63e5d54b6bb73491df7c2465a7a4d9
phicks22 <65268541+phicks22@users.noreply.github.com>
2022-12-20 11:37:27 -0700

    modularized genotype generation

--- sample_global_ancestry.R
+++ sample_global_ancestry.R
@@ -6,8 +6,31 @@ library(tidyr)
 sample_aa_ancestry <- sample(1:9, 320, replace = TRUE)
 haplotype_1 <- list()
 haplotype_2 <- list()
-risk_allele_freqs <- sample(1:9, 320, replace = TRUE) / 10
+ea_risk_allele_freqs <- sample(1:9, 320, replace = TRUE) / 10
+aa_risk_allele_freqs <- sample(1:9, 320, replace = TRUE) / 10
 all_indiv <- list() # To be a list of dfs for each individual
+
+
+generate_genotype <- function(chr_ancestry, aa_risk, ea_risk) {
+  genotypeCount <- 0
+  for (k in 1:2) {
+    
+    if (chr_ancestry[k] == "a") {
+      alleleChance <- runif(1, min=0, max=10)
+      if (alleleChance <= aa_risk) {
+        genotypeCount = genotypeCount + 1
+      }
+    }
+    else {
+      alleleChance <- runif(1, min=0, max=10)
+      if (alleleChance <= ea_risk) {
+        genotypeCount = genotypeCount + 1
+      }
+    }
+  }
+  
+  return(genotypeCount)
+}
 
 # Generate random alleles for N=100 individuals
 for (i in 1:100) {
@@ -29,25 +52,19 @@ for (i in 1:100) {
         chromosomes[chromosome] <- "e"
       }
     }
+    
     # 2 <- risk genotype
     # 1 <- heterozygous
     # 0 <- wild type
-    genotypeCount <- 0
-    for (k in 1:2) {
-      alleleChance <- runif(1, min=0, max=10)
-      if (alleleChance <= risk_allele_freqs[j]) {
-        genotypeCount = genotypeCount + 1
-      }
-    }
+    genotypeCount <- generate_genotype(chr_ancestry = chromosomes, 
+                                      aa_risk = aa_risk_allele_freqs[j], 
+                                       ea_risk = aa_risk_allele_freqs[j])
+    
     # Append genotype to simulated individual 
     snps_genotype[j] <- genotypeCount
   }
+  # Add genotype scores to df
+  individual$snps <- snps_genotype
+  # TODO calculate grs. Need dosage data
+  # TODO add grs scores to df
 }
-
-# haplotype_1[[j]] = alleles[[sample(1:length(alleles), 1)]]
-# haplotype_2[[j]] = alleles[[sample(1:length(alleles), 1)]]
-# }
-# # Assign randomly selected snps to an individual
-# individual$hap1 <- haplotype_1
-# individual$hap2 <- haplotype_2
-
