255810edef5d6c65c8c3e0af1758a005bc6ef880
phicks22 <65268541+phicks22@users.noreply.github.com>
2023-01-06 14:40:00 -0700

    changed arg names & read data

--- read_grs_scores.R
+++ read_grs_scores.R
@@ -2,11 +2,17 @@
 library("optparse")
 
 option_list = list(
-  make_option(c("-gf", "--grs_file"), type="character", default=NULL, 
+  make_option(c("-g", "--grs_file"), type="character", default=NULL, 
               help="grs scores dataset file name", metavar="character"),
-  make_option(c("-aa", "--aa_ancestry"), type="character", default=NULL, 
-              help="african ancestry dataset file name", metavar="character")
+  make_option(c("-v", "--variables"), type="character", default=NULL, 
+              help="alternative variables dataset file name", metavar="character")
 )
 
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
+
+grs_file <- opt[1]
+variables_file <- opt[2]
+
+grs_scores <- read.table(grs_file, sep=",")
+variables <- read.table(variables_file, sep=",")
