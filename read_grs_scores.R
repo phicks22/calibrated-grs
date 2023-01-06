e4594856124c901d8d0684caa92568efc87d3b8f
phicks22 <65268541+phicks22@users.noreply.github.com>
2023-01-06 14:28:58 -0700

    Added ancestry file arg

--- read_grs_scores.R
+++ read_grs_scores.R
@@ -2,10 +2,10 @@
 library("optparse")
 
 option_list = list(
-  make_option(c("-f", "--file"), type="character", default=NULL, 
-              help="dataset file name", metavar="character"),
-  make_option(c("-o", "--out"), type="character", default="out.txt", 
-              help="output file name [default= %default]", metavar="character")
+  make_option(c("-gf", "--grs_file"), type="character", default=NULL, 
+              help="grs scores dataset file name", metavar="character"),
+  make_option(c("-aa", "--aa_ancestry"), type="character", default=NULL, 
+              help="african ancestry dataset file name", metavar="character")
 )
 
 opt_parser = OptionParser(option_list=option_list)
